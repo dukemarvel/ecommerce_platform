4146e58eadc2c098ccf8a90b18fdd8a01e33eb64
Joseph BlackDuke <josephblackduke@icloud.com>
2024-07-11 15:48:29 +0100

    Correct import statement for filters in ProductViewSet

--- products/views.py
+++ products/views.py
@@ -2,13 +2,15 @@ from rest_framework import viewsets
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['category', 'name']
     search_fields = ['name', 'description']
     ordering_fields = ['name', 'price']
