53ab75df29ced9e4dd60d38ff2f249780ba6f407
Joseph BlackDuke <josephblackduke@icloud.com>
2024-07-16 17:00:49 +0100

    Customize admin interface for Product and Category models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'unitPrice', 'quantityInStock')
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
